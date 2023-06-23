3a9b366017871d7ac818e683218314d0d0af6396
James Kawakami <jkawakami@Jamess-MacBook-Air.local>
2023-06-22 21:41:53 -0700

    added numbered list to choose MLB team

--- team-schedule.py
+++ team-schedule.py
@@ -2,42 +2,50 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import date
 
+def displayTeams(dict):
+    counter = 0
+    record = {}
+    for team in dict:
+        counter += 1
+        record[counter] = team
+        print("%s. %s" % (counter, team))
+    return record
+
+def chooseTeam(record, teams):
+    print("\nPick a number:")
+    num = input()
+    num = int(num)
+    if num in record:        
+        print(record[num])
+        return record[num], teams[record[num]]
+
 today = date.today()
 format_today = today.strftime("%a, %b %d")
 
+baseURL="https://www.espn.com"
 MLB="https://www.espn.com/mlb/teams"
 page = requests.get(MLB)
 soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find("h2")
-t = results.contents[0].split(" ")
-teams = []
-i=0
-while i<30:
-    results = results.find_next('h2')
-    t = results.contents[0].split(" ")
+results = soup.findAll("h2")
+teams = {}
+for res in results:
+    t = res.contents[0].split(" ")
     if len(t)>3:
-        break
+        continue
     else:
-        teams.append(results.contents[0])
-    
-    i+=1
+        teams[res.contents[0]]=res.parent.attrs["href"]
 
-print(teams)
+record = displayTeams(teams)
+team, teamlink = chooseTeam(record, teams)
+schedulelink = teamlink[:10] + 'schedule/' + teamlink[10:]
 
-team = input("Enter Team: ")
-if team == "UCLA" or team=="ucla":
-    URL = "https://www.espn.com/mens-college-basketball/team/schedule/_/id/26/ucla-bruins"
-elif team == "dodgers" or team=="Dodgers":
-    URL = "https://www.espn.com/mlb/team/schedule/_/name/lad/los-angeles-dodgers"
-#URL = "https://realpython.github.io/fake-jobs/"
-
-
+URL = baseURL+schedulelink
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find("td", string=format_today)
 if results is None:
-    print(team," doesn't play today")
+    print(team," zilch")
 else:
     print(team," plays today, " + results.text.strip() + " " + results.next_sibling.text)
 
